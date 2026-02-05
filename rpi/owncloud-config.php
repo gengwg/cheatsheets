6ff4165b1d317f0bf4c78c6d1eb8aaf79c61fb22
gengwg <gengwg@users.noreply.github.com>
2026-02-04 19:01:51 -0800

    raspberry pi configs

---
+++ rpi/owncloud-config.php
@@ -0,0 +1,28 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocmxltqhktqj',
+  'passwordsalt' => 'PaXkwVaBUZpoqrdbVSxm5NUZB9OyXo',
+  'secret' => 'zEZFWNo6JriqVNKtALmCNSWt27Jz8AfUrRReYj3G84/n37n7',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.104',
+    1 => '162.228.89.113',
+    2 => 'rpi',
+    3 => '192.168.1.76',
+    4 => 'raspberrypi.local'
+  ),
+  'datadirectory' => '/media/pi/Touro/nextcloud/',
+  'overwrite.cli.url' => 'http://192.168.1.104/nextcloud',
+  'dbtype' => 'mysql',
+  'version' => '13.0.5.2',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'ncuser',
+  'dbpassword' => 'raindrop',
+  'installed' => true,
+  'filelocking.enabled' => false,
+
+);
+
